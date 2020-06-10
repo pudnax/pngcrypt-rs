a06ace866527a40ca42354b205894546ca84818f
Alex Komissarov <k.a.komissar@gmail.com>
2020-06-10 19:35:49 +0500

    Better project managment

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+mod args;
+mod chunk;
+mod chunk_type;
+mod commands;
+mod error;
+mod png;
+
+pub use error::{Error, Result};
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-mod args;
-mod chunk;
-mod chunk_type;
-mod commands;
-mod png;
-
-pub type Error = Box<dyn std::error::Error>;
-pub type Result<T> = std::result::Result<T, Error>;
+use pngme::Result;
 
 fn main() -> Result<()> {
     println!("Hello, world!");
