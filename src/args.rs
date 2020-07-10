e84414c344e3237c6f239aa67f12cc6cbf1fe725
Alex Komissarov <k.a.komissar@gmail.com>
2020-07-10 10:04:08 +0500

    Stoped to select enum as a suitable structure for commands and added documentation.

--- src/args.rs
+++ src/args.rs
@@ -1,37 +1,52 @@
-use crate::{Error, Result};
-use std::{path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 use structopt::StructOpt;
-
-#[derive(Debug)]
-enum Mode {
-    Encode,
-    Decode,
-    Print,
-}
-
-impl FromStr for Mode {
-    type Err = Error<'static>;
-    fn from_str(s: &str) -> Result<Self> {
-        match s {
-            "encode" => Ok(Mode::Encode),
-            "decode" => Ok(Mode::Decode),
-            "print" => Ok(Mode::Print),
-            _ => Err(Error::Custom("Parse error")),
-        }
-    }
-}
 
 /// Command line program that lets you hide secret messages in PNG files.
 #[derive(Debug, StructOpt)]
 #[structopt(name = "pngme")]
-pub struct Opt {
-    /// Application mode.
-    mode: Mode,
+pub enum Opt {
+    /// Encodes a message into a PNG file and saves the result
+    Encode(EncodeArgs),
+    /// Searches for a message hidden in a PNG file and prints the message if one is found
+    Decode(DecodeArgs),
+    /// Removes a chunk from a PNG file and saves the result
+    Remove(RemoveArgs),
+    /// Prints all of the chunks in a PNG file
+    Print(PrintArgs),
+}
+
+#[derive(Debug, StructOpt)]
+pub struct EncodeArgs {
     /// Path to file
     #[structopt(parse(from_os_str))]
-    input: PathBuf,
+    pub input: PathBuf,
     /// Chunk type
-    chunk_type: Option<String>,
+    pub chunk_type: String,
     /// Secret message
-    message: Option<String>,
+    pub message: String,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct DecodeArgs {
+    /// Path to file
+    #[structopt(parse(from_os_str))]
+    pub input: PathBuf,
+    /// Chunk type
+    pub chunk_type: String,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct RemoveArgs {
+    /// Path to file
+    #[structopt(parse(from_os_str))]
+    pub input: PathBuf,
+    /// Chunk type
+    pub chunk_type: String,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct PrintArgs {
+    /// Path to file
+    #[structopt(parse(from_os_str))]
+    pub input: PathBuf,
 }
