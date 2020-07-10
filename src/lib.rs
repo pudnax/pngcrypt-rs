9ecff3bb6a1e82bfb649f5851b3b83e5a0428f87
Alex Komissarov <k.a.komissar@gmail.com>
2020-07-10 10:07:16 +0500

    Finish!

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ mod commands;
 mod error;
 pub mod png;
 
+pub use chunk::Chunk;
+pub use chunk_type::ChunkType;
+pub use png::Png;
+
 pub const BYTE_SIZE: usize = 4;
 
 pub use error::{Error, Result};
