3c7c1c16523bb53530c5fe463b7111a822e83baa
Alex Komissarov <k.a.komissar@gmail.com>
2020-07-10 14:21:01 +0500

    Added tests to file IO and the decode dunction returns tre Err on fail now.

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,6 @@
 use crate::{
-    args::{DecodeArgs, EncodeArgs, PrintArgs, RemoveArgs},
-    Chunk, ChunkType, Png, Result,
+    args::{DecodeArgs, EncodeArgs, RemoveArgs},
+    Chunk, ChunkType, Error, Png, Result,
 };
 use std::{
     convert::TryInto,
@@ -18,20 +18,20 @@ fn take_png<P: AsRef<Path>>(input: P) -> Result<Png> {
     Ok(buffer.as_slice().try_into()?)
 }
 
-pub fn encode(args: EncodeArgs) -> Result<()> {
-    let mut png = take_png(&args.input)?;
+pub fn encode<S: AsRef<Path>>(input: S, args: EncodeArgs) -> Result<()> {
+    let mut png = take_png(&input)?;
     png.append_chunk(Chunk::new(
         ChunkType::from_str(&args.chunk_type)?,
         args.message.into_bytes(),
     ));
 
-    let mut file = std::fs::File::create(args.input)?;
+    let mut file = std::fs::File::create(input)?;
     file.write_all(&png.as_bytes())?;
     Ok(())
 }
 
-pub fn decode(args: DecodeArgs) -> Result<()> {
-    let png = take_png(&args.input)?;
+pub fn decode<S: AsRef<Path>>(input: S, args: DecodeArgs) -> Result<()> {
+    let png = take_png(&input)?;
     if let Some(chunk) = png.chunk_by_type(&args.chunk_type) {
         println!(
             "Hidden message in the chunk {}: '{}'",
@@ -39,30 +39,115 @@ pub fn decode(args: DecodeArgs) -> Result<()> {
             chunk.data_as_string()?
         );
     } else {
-        println!("Unable to decode chunk");
+        return Err(Error::Custom("Unable to decode chunk"));
     }
     Ok(())
 }
 
-pub fn remove(args: RemoveArgs) -> Result<()> {
-    let mut png = take_png(&args.input)?;
+pub fn remove<S: AsRef<Path>>(input: S, args: RemoveArgs) -> Result<()> {
+    let mut png = take_png(&input)?;
     png.remove_chunk(&args.chunk_type)?;
 
-    let mut file = std::fs::File::create(args.input)?;
+    let mut file = std::fs::File::create(input)?;
     file.write_all(&png.as_bytes())?;
     Ok(())
 }
 
-pub fn print(args: PrintArgs) -> Result<()> {
-    let png = take_png(&args.input)?;
-    println!(
-        "File: {}, Size: {}",
-        args.input.display(),
-        png.as_bytes().len()
-    );
+pub fn print(input: &Path) -> Result<()> {
+    let png = take_png(&input)?;
+    println!("File: {}, Size: {}", input.display(), png.as_bytes().len());
     for (i, chunk) in png.chunks().iter().enumerate() {
         print!("\n({})", i + 1);
         print!("{}", chunk);
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn make_copy_of_file<'a>() -> &'a Path {
+        let input = Path::new("copy.png");
+        std::fs::File::create(&input).unwrap();
+        std::fs::copy("pic.png", &input).unwrap();
+        input
+    }
+
+    #[test]
+    fn test_encode() {
+        let input = make_copy_of_file();
+        let chunk_type = "RuST".to_string();
+        let message = "Message".to_string();
+        let args = EncodeArgs {
+            chunk_type,
+            message,
+        };
+        let res = encode(input, args);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_decode() {
+        let input = make_copy_of_file();
+        let chunk_type = "RuST".to_string();
+        let message = "Message".to_string();
+        let args = EncodeArgs {
+            chunk_type,
+            message,
+        };
+        let res = encode(input, args);
+        assert!(res.is_ok());
+        let chunk_type = "RuST".to_string();
+        let args = DecodeArgs { chunk_type };
+        let res = decode(input, args);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_remove() {
+        let input = make_copy_of_file();
+        let chunk_type = "RuST".to_string();
+        let message = "Message".to_string();
+        let args = EncodeArgs {
+            chunk_type,
+            message,
+        };
+        let res = encode(input, args);
+        assert!(res.is_ok());
+        let chunk_type = "RuST".to_string();
+        let args = RemoveArgs { chunk_type };
+        let res = remove(input, args);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_print() {
+        let input = make_copy_of_file();
+        let res = print(&input);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_all_one() {
+        let input = make_copy_of_file();
+        let chunk_type = "RuST".to_string();
+        let message = "Message".to_string();
+        let args = EncodeArgs {
+            chunk_type,
+            message,
+        };
+        let res = encode(input, args);
+        assert!(res.is_ok());
+        let chunk_type = "RuST".to_string();
+        let args = DecodeArgs { chunk_type };
+        let res = decode(input, args);
+        assert!(res.is_ok());
+        let chunk_type = "RuST".to_string();
+        let args = RemoveArgs { chunk_type };
+        let res = remove(input, args);
+        assert!(res.is_ok());
+        let res = print(&input);
+        assert!(res.is_ok());
+    }
 }
